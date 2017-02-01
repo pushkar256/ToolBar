7631095a2bd69a58951ddaef212e83143017263d
pushkar256 <pushkar.codebrew@gmail.com>
2017-02-01 18:54:53 +0530

    Initial commit

---
+++ app/src/main/java/com/example/cbluser22/toolbar/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.cbluser22.toolbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+                onBackPressed();
+                break;
+            
+
+
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+}
